df94d5f465d81c66a598629e771b2e507bf8a0bb
JeyasreePradeep <jeyasreedj@gmail.com>
2023-02-22 12:29:07 +0530

    modified

--- src/org/tcs/FaceBook.java
+++ src/org/tcs/FaceBook.java
@@ -1,4 +1,17 @@
 package org.tcs;
 public class FaceBook {
-
+	private void fbUrl(String name) {
+		System.out.println("Fb url is "+name);
+		System.out.println("facebook updated");
+	}
+	
+	private void fbLogin(String id, String pWord) {
+		System.out.println("user id is" +id);
+		System.out.println("passWord is "+pWord);
+	}
+	public static void main(String[] args) {
+		FaceBook f=new FaceBook();
+		f.fbUrl("www.facebook.com");
+		f.fbLogin("jeyasree", "sree");
+	}
 }
